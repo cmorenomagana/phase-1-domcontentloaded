77cb5a2eab9ce2ce75807ded94a31c98bfffaa69
Cuitlahuatl <cmorenomagana@gmail.com>
2023-11-20 16:21:05 -0600

    DOM Content Loaded

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const elem = document.getElementById("text")
+    elem.textContent = "This is really cool!"
+});
